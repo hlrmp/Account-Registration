02f63cabfc9ff16679861e504f7401f7131c48f0
deguzman.238962 <deguzman.238962@LAB-B-PC07>
2023-09-08 19:31:53 +0800

    Add project files.

---
+++ Account Registration/Form1.Designer.cs
@@ -0,0 +1,226 @@
+﻿namespace Account_Registration
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            label1 = new Label();
+            label2 = new Label();
+            textBoxsn = new TextBox();
+            txtln = new TextBox();
+            comboBox1program = new ComboBox();
+            label3 = new Label();
+            label4 = new Label();
+            txtfn = new TextBox();
+            label5 = new Label();
+            txtmn = new TextBox();
+            label6 = new Label();
+            txtage = new TextBox();
+            label7 = new Label();
+            txtcnt = new TextBox();
+            label8 = new Label();
+            ttxtaddress = new TextBox();
+            SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(55, 52);
+            label1.Name = "label1";
+            label1.Size = new Size(101, 15);
+            label1.TabIndex = 0;
+            label1.Text = "Student Number :";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(307, 52);
+            label2.Name = "label2";
+            label2.Size = new Size(59, 15);
+            label2.TabIndex = 1;
+            label2.Text = "program :";
+            // 
+            // textBoxsn
+            // 
+            textBoxsn.Location = new Point(56, 70);
+            textBoxsn.Name = "textBoxsn";
+            textBoxsn.Size = new Size(100, 23);
+            textBoxsn.TabIndex = 2;
+            // 
+            // txtln
+            // 
+            txtln.Location = new Point(55, 181);
+            txtln.Name = "txtln";
+            txtln.Size = new Size(100, 23);
+            txtln.TabIndex = 3;
+            // 
+            // comboBox1program
+            // 
+            comboBox1program.FormattingEnabled = true;
+            comboBox1program.Location = new Point(307, 70);
+            comboBox1program.Name = "comboBox1program";
+            comboBox1program.Size = new Size(175, 23);
+            comboBox1program.TabIndex = 4;
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Location = new Point(56, 152);
+            label3.Name = "label3";
+            label3.Size = new Size(69, 15);
+            label3.TabIndex = 5;
+            label3.Text = "Last Name :";
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Location = new Point(233, 152);
+            label4.Name = "label4";
+            label4.Size = new Size(70, 15);
+            label4.TabIndex = 6;
+            label4.Text = "First Name :";
+            // 
+            // txtfn
+            // 
+            txtfn.Location = new Point(233, 181);
+            txtfn.Name = "txtfn";
+            txtfn.Size = new Size(100, 23);
+            txtfn.TabIndex = 7;
+            // 
+            // label5
+            // 
+            label5.AutoSize = true;
+            label5.Location = new Point(403, 152);
+            label5.Name = "label5";
+            label5.Size = new Size(85, 15);
+            label5.TabIndex = 8;
+            label5.Text = "Middle Name :";
+            // 
+            // txtmn
+            // 
+            txtmn.Location = new Point(403, 181);
+            txtmn.Name = "txtmn";
+            txtmn.Size = new Size(100, 23);
+            txtmn.TabIndex = 9;
+            // 
+            // label6
+            // 
+            label6.AutoSize = true;
+            label6.Location = new Point(55, 252);
+            label6.Name = "label6";
+            label6.Size = new Size(32, 15);
+            label6.TabIndex = 10;
+            label6.Text = "age :";
+            // 
+            // txtage
+            // 
+            txtage.Location = new Point(55, 282);
+            txtage.Name = "txtage";
+            txtage.Size = new Size(100, 23);
+            txtage.TabIndex = 11;
+            // 
+            // label7
+            // 
+            label7.AutoSize = true;
+            label7.Location = new Point(233, 252);
+            label7.Name = "label7";
+            label7.Size = new Size(102, 15);
+            label7.TabIndex = 12;
+            label7.Text = "Contact Number :";
+            // 
+            // txtcnt
+            // 
+            txtcnt.Location = new Point(235, 282);
+            txtcnt.Name = "txtcnt";
+            txtcnt.Size = new Size(143, 23);
+            txtcnt.TabIndex = 13;
+            // 
+            // label8
+            // 
+            label8.AutoSize = true;
+            label8.Location = new Point(55, 347);
+            label8.Name = "label8";
+            label8.Size = new Size(55, 15);
+            label8.TabIndex = 14;
+            label8.Text = "Address :";
+            // 
+            // ttxtaddress
+            // 
+            ttxtaddress.Location = new Point(55, 365);
+            ttxtaddress.Multiline = true;
+            ttxtaddress.Name = "ttxtaddress";
+            ttxtaddress.Size = new Size(433, 91);
+            ttxtaddress.TabIndex = 15;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(551, 482);
+            Controls.Add(ttxtaddress);
+            Controls.Add(label8);
+            Controls.Add(txtcnt);
+            Controls.Add(label7);
+            Controls.Add(txtage);
+            Controls.Add(label6);
+            Controls.Add(txtmn);
+            Controls.Add(label5);
+            Controls.Add(txtfn);
+            Controls.Add(label4);
+            Controls.Add(label3);
+            Controls.Add(comboBox1program);
+            Controls.Add(txtln);
+            Controls.Add(textBoxsn);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            FormBorderStyle = FormBorderStyle.Fixed3D;
+            Name = "Form1";
+            Text = "FrmRegistration";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Label label1;
+        private Label label2;
+        private TextBox textBoxsn;
+        private TextBox txtln;
+        private ComboBox comboBox1program;
+        private Label label3;
+        private Label label4;
+        private TextBox txtfn;
+        private Label label5;
+        private TextBox txtmn;
+        private Label label6;
+        private TextBox txtage;
+        private Label label7;
+        private TextBox txtcnt;
+        private Label label8;
+        private TextBox ttxtaddress;
+    }
+}
